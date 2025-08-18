828f47ec894031864c4fd1f02b140de7ba2efe5e
nikloynes <nl1676@nyu.edu>
2025-08-18 15:31:37 +0100

    fixes to the data model

--- app/data_models.py
+++ app/data_models.py
@@ -1,5 +1,6 @@
 """Data models and associated methods used to specify input and output data."""
 
+import re
 from typing import Any, Self
 
 from destiny_sdk.enhancements import Authorship, EnhancementType
@@ -12,7 +13,7 @@ from destiny_sdk.identifiers import (
 )
 from destiny_sdk.references import Reference, ReferenceFileInput
 from loguru import logger
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field, field_validator, model_validator
 from pydantic_extra_types.isbn import ISBN
 
 
@@ -27,6 +28,7 @@ class Paper(BaseModel):
     openalex_id: OpenAlexIdentifier | None = Field(default=None)
     pubmed_id: PubMedIdentifier | None = Field(default=None)
     isbn: ISBN | None = Field(default=None)
+    issn: str | None = Field(default=None)
     title: str | None = Field(default=None)
     authors: list[Authorship] | None = Field(default=None)
     year: int | None = Field(default=None)
@@ -34,6 +36,16 @@ class Paper(BaseModel):
     publisher: str | None = Field(default=None)
     pages: tuple[int, int] | None = Field(default=None)
     abstract: str | None = Field(default=None)
+
+    @classmethod
+    @field_validator("issn", mode="before")
+    def check_valid_issn(cls, v: str | None) -> str | None:
+        """Ensure issn (if present) is valid."""
+        if v:
+            issn_regex = re.compile(r"^[0-9]{4}-[0-9]{3}[0-9X]$.")
+            if issn_regex.match(v):
+                return v
+        return None
 
     @model_validator(mode="after")
     def check_for_non_missing(self) -> Self:
@@ -89,21 +101,43 @@ def convert_ref_to_paper(ref: ReferenceFileInput | Reference) -> Paper:
     )
     # get bib enhancement, for title/author/year/publisher/etc.
     bib_enh = None
+    loc_enh = None
+    loc_enh_extra = None
     if ref.enhancements:
-        for enh in ref.enhancements:
+        logger.debug(f"n enhancements: {len(ref.enhancements)}")
+        for i, enh in enumerate(ref.enhancements):
+            logger.debug(f"on enhancement {i}.")
+            logger.debug(f"enhancement: {enh}")
             enh_content = getattr(enh, "content", None)
             enh_type = getattr(enh_content, "enhancement_type", None)
+            if enh_type == EnhancementType.LOCATION and enh_content:
+                loc_enh = getattr(enh_content, "locations", [])
+                continue
+
             if enh_type == EnhancementType.BIBLIOGRAPHIC:
                 bib_enh = enh_content if hasattr(enh, "content") else None
-                break
+                continue
 
     title = getattr(bib_enh, "title", None) if bib_enh else None
     authors = getattr(bib_enh, "authorship", None) if bib_enh else None
     year = getattr(bib_enh, "publication_year", None) if bib_enh else None
     publisher = getattr(bib_enh, "publisher", None) if bib_enh else None
-    journal = None
+    journal_bib = getattr(bib_enh, "journal", None) if bib_enh else None
+
+    logger.debug(loc_enh)
+    logger.debug(loc_enh_extra)
+
+    loc_enh_extra = getattr(loc_enh[0], "extra", {}) if loc_enh else None
+    journal_loc = loc_enh_extra.get("display_name", None) if loc_enh_extra else None
+    issn_list = loc_enh_extra.get("issn", None) if loc_enh_extra else None
+    issn = None
+    if issn_list:
+        issn = issn_list[0] if isinstance(issn_list, list) else issn_list
+
     abstract = None
     pages = None
+
+    journal = journal_bib if journal_bib is not None else journal_loc
 
     # search for abstract
     if ref.enhancements:
@@ -122,6 +156,7 @@ def convert_ref_to_paper(ref: ReferenceFileInput | Reference) -> Paper:
         openalex_id=openalex_id,
         pubmed_id=pubmed_id,
         isbn=isbn,
+        issn=issn,
         title=title,
         authors=authors,
         year=year,
